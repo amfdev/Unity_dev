b27e4f237bb0617dc219b3bc97453c447a497481
OvchinnikovDmitrii <ovchinnikov.dmitrii@gmail.com>
2018-09-06 01:22:50 +0300

    Additional  log string.

--- Scripts/Samples/build_unity_project.bat
+++ Scripts/Samples/build_unity_project.bat
@@ -1,6 +1,7 @@
 echo off
 set curDir=%cd%
 set unity="%PROGRAMFILES%/Unity/Editor/Unity.exe"
+set unity="D:\\Programs\\Unity\\Editor/Unity.exe"
 
 set projectPath=%1
 set build_name=%~n1
@@ -10,6 +11,7 @@ echo "build name = " %build_name%
 set win64=-buildWindows64Player %cd%\..\..\Bin\%build_name%\win64\%build_name%.exe
 
 echo building %build_name% to %cd%\build\%build_name%\win64\%build_name%.exe
+echo %unity% -batchmode -quit -projectPath %projectPath%  %win64%
 %unity% -batchmode -quit -projectPath %projectPath%  %win64%
 
 cd %curDir%
